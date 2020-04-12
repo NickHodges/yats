1bd76239ec840a1724dab68c6f2ce677ecf93720
Nick Hodges <nickhodges@gmail.com>
2020-04-12 07:50:02 -0400

    protect Todos from unloggedin users

--- src/util/session-store.util.ts
+++ src/util/session-store.util.ts
@@ -17,6 +17,10 @@ class SessionStore {
     const session: Session = this.sessions[sessionId];
     return session && session.isValid();
   }
+
+  destroySession(sessionId: string): void { 
+    delete this.sessions[sessionId];
+  }
 }
 
 export const sessionStore = new SessionStore();
